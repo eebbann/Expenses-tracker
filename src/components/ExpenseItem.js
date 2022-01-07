2749ad53d44eff1875dc2c2f03dea264ce821232
eebann <ebanemmanuel@live.com>
2022-01-07 13:14:37 -0600

    expense tracker

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+
+	
+	return (
+	<div className='expense-item'> 
+		<div>March 21st 2021</div>
+		<div className= "expense-item__description">
+	   	<p> The Hello World </p>
+			<div className='expense-item__price'>$1234</div>
+		</div>
+		</div>
+	)
+}
+
+export default ExpenseItem;
